c048fb266ad719c914e64c835623d39f26b5cb4d
golkedj <golkedj@gmail.com>
2020-02-14 14:44:43 -0700

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedSubscription = 'advanced';
+  selectedSubscription = 'Advanced';
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(form.value);
   }
 }
